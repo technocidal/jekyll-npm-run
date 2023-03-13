6eacb2f7a85e6321bf6d8e48d4390a801dc70cad
Johannes Ebeling <14994778+technocidal@users.noreply.github.com>
2023-03-13 20:44:30 +0100

    🎨 Formatting
    Co-authored-by: Henry Thoma <thoma@sipgate.de>

--- lib/jekyll-npm-run.rb
+++ lib/jekyll-npm-run.rb
@@ -1,8 +1,7 @@
-Jekyll::Hooks.register :site, :after_init do |site|
-    script = site.config["jekyll-npm-run"][0]["script"]
-    
-    Jekyll::Hooks.register :site, :post_write do |site|
-      system "npm run #{script}"
-    end
-  end
+Jekyll::Hooks.register(:site, :after_init) do |site|
+  script = site.config["jekyll-npm-run"][0]["script"]
 
+  Jekyll::Hooks.register(:site, :post_write) do |site|
+    system("npm run #{script}")
+  end
+end
